caacd1c8c2ee8b5589983b5fdf40d45019c98955
bweir27 <bweir27@gmail.com>
2020-05-08 19:02:17 -0600

    Updates

---
+++ precache-manifest.99df0d3a4bb0b1be05ff9e5eae0330f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9a7cb668fe67fef4bc93e81239e594c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c7c05a226fd15a872c6",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "00e02313f4e87f324c92",
+    "url": "/static/js/2.f692bf04.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.f692bf04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c7c05a226fd15a872c6",
+    "url": "/static/js/main.a4af0dc1.chunk.js"
+  },
+  {
+    "revision": "967a46d8b67e4e01db1f",
+    "url": "/static/js/runtime-main.7e4b9196.js"
+  }
+]);
